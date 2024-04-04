20c61fa1540b93f93f445683322051847fddd8d2
ulog4l <ulog4l@gmail.com>
2024-04-04 15:27:02 +0200

    file upload

---
+++ firmware/zodj_d/keymaps/vial/config.h
@@ -0,0 +1,23 @@
+// Copyright 2024 ulog4l (@ulog4l)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0xEA, 0xF5, 0x91, 0x10, 0xFA, 0xF8, 0x73, 0x58}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 0 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
